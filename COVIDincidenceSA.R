68d611f9166b09b84435fa1dca43c0b32299852c
belayb <belaybirlie.yimer@manchester.ac.uk>
2021-02-18 07:06:22 +0000

    Forecasting

--- COVIDincidenceSA.R
+++ COVIDincidenceSA.R
@@ -81,7 +81,7 @@ p_ar1_inc<-ggplot(newdata_ar1,
                   geom_ribbon(aes(ymin=lower, ymax=upper), fill='blue', alpha=0.6) +
                   geom_line(aes(y=mean, x=date2)) +xlab("Date")+
                   ylab("Daily observed and fitted cases")+
-                  labs(title = "Negaive-Binomial with AR(1) model" )+
+                  labs(title = "Negative-Binomial with AR(1) model" )+
                   theme_bw()
 
 
@@ -92,7 +92,7 @@ p_rw1_inc<-ggplot(newdata_rw1, aes(y=mean, x=date2)) +
   geom_point(aes(y=Dialy_cases, x=date2)) +
   geom_ribbon(aes(ymin=lower, ymax=upper), fill='blue', alpha=0.6) +
   geom_line(aes(y=mean, x=date2)) +xlab("Date")+ylab("Daily observed and fitted cases")+
-  labs(title = "Negaive-Binomial with RW(1) model" )+
+  labs(title = "Negative-Binomial with RW(1) model" )+
   theme_bw()
 
 p_ar1_cum<-newdata_ar1%>%
@@ -104,7 +104,7 @@ p_ar1_cum<-newdata_ar1%>%
   geom_ribbon(aes(ymin=Cumm_fitted_lower, ymax=Cumm_fitted_upper), fill='blue', alpha=0.6) +
   geom_line(aes(y=Cumm_fitted, x=date2)) +xlab("Date")+
   ylab("cummulative observed and fitted cases")+
-  labs(title = "Negaive-Binomial with AR(1) model" )+
+  labs(title = "Negative-Binomial with AR(1) model" )+
   theme_bw()
 p_rw1_cum<-newdata_rw1%>%
   mutate(Cumm_fitted=cumsum(mean), Cumm_fitted_lower=cumsum(lower),
@@ -115,7 +115,7 @@ p_rw1_cum<-newdata_rw1%>%
   geom_ribbon(aes(ymin=Cumm_fitted_lower, ymax=Cumm_fitted_upper), fill='blue', alpha=0.6) +
   geom_line(aes(y=Cumm_fitted, x=date2)) +xlab("Date")+
   ylab("cummulative observed and fitted cases")+
-  labs(title = "Negaive-Binomial with RW(1) model" )+
+  labs(title = "Negative-Binomial with RW(1) model" )+
   theme_bw()
 
 #plot of incidence over time 
@@ -126,11 +126,125 @@ p_ar1_cum/p_rw1_cum
 
 
 # Do Forcasting 
+tail(sa_cov_dat)
+sa_cov_dat$Dialy_cases3<-sa_cov_dat$Dialy_cases
+sa_cov_dat$Dialy_cases5<-sa_cov_dat$Dialy_cases
+sa_cov_dat$Dialy_cases7<-sa_cov_dat$Dialy_cases
+
+sa_cov_dat$Dialy_cases3[sa_cov_dat$time>339]<-NA
+sa_cov_dat$Dialy_cases5[sa_cov_dat$time>337]<-NA
+sa_cov_dat$Dialy_cases7[sa_cov_dat$time>335]<-NA
+
+fomula_ar1_3<-Dialy_cases3 ~ 1 + f(time, model = "ar1",
+                                hyper = list(prec = list(param = c(0.1, 0.1))))
+fomula_rw1_3<-Dialy_cases3 ~ 1 + f(time, model = "rw1",
+                                hyper = list(prec = list(param = c(0.1, 0.1))))
+fomula_ar1_5<-Dialy_cases5 ~ 1 + f(time, model = "ar1",
+                                 hyper = list(prec = list(param = c(0.1, 0.1))))
+fomula_rw1_5<-Dialy_cases5 ~ 1 + f(time, model = "rw1",
+                                 hyper = list(prec = list(param = c(0.1, 0.1))))
+fomula_ar1_7<-Dialy_cases7 ~ 1 + f(time, model = "ar1",
+                                 hyper = list(prec = list(param = c(0.1, 0.1))))
+fomula_rw1_7<-Dialy_cases7 ~ 1 + f(time, model = "rw1",
+                                 hyper = list(prec = list(param = c(0.1, 0.1))))
+
+nb.ar1_3 <- inla(fomula_ar1_3,                   
+               data = sa_cov_dat,
+               family = "nbinomial",
+               control.predictor = list(compute = TRUE, link = 1),
+               control.compute=list(cpo=TRUE,waic=TRUE,dic=TRUE))
+nb.rw1_3 <- inla(fomula_rw1_3,                   
+               data = sa_cov_dat,
+               family = "nbinomial",
+               control.predictor = list(compute = TRUE, link = 1),
+               control.compute=list(cpo=TRUE,waic=TRUE,dic=TRUE))
+nb.ar1_5 <- inla(fomula_ar1_5,                   
+                 data = sa_cov_dat,
+                 family = "nbinomial",
+                 control.predictor = list(compute = TRUE, link = 1),
+                 control.compute=list(cpo=TRUE,waic=TRUE,dic=TRUE))
+nb.rw1_5 <- inla(fomula_rw1_5,                   
+                 data = sa_cov_dat,
+                 family = "nbinomial",
+                 control.predictor = list(compute = TRUE, link = 1),
+                 control.compute=list(cpo=TRUE,waic=TRUE,dic=TRUE))
+nb.ar1_7 <- inla(fomula_ar1_7,                   
+                 data = sa_cov_dat,
+                 family = "nbinomial",
+                 control.predictor = list(compute = TRUE, link = 1),
+                 control.compute=list(cpo=TRUE,waic=TRUE,dic=TRUE))
+nb.rw1_7 <- inla(fomula_rw1_7,                   
+                 data = sa_cov_dat,
+                 family = "nbinomial",
+                 control.predictor = list(compute = TRUE, link = 1),
+                 control.compute=list(cpo=TRUE,waic=TRUE,dic=TRUE))
+
+nb.rw1_3_pred<-nb.rw1_3$summary.fitted.values[,c(1,3,5)]
+nb.rw1_5_pred<-nb.rw1_5$summary.fitted.values[,c(1,3,5)]
+nb.rw1_7_pred<-nb.rw1_7$summary.fitted.values[,c(1,3,5)]
+nb.ar1_3_pred<-nb.ar1_3$summary.fitted.values[,c(1,3,5)]
+nb.ar1_5_pred<-nb.ar1_5$summary.fitted.values[,c(1,3,5)]
+nb.ar1_7_pred<-nb.ar1_7$summary.fitted.values[,c(1,3,5)]
+
+nb.rw1_3_pred <- reshape:::rename(nb.rw1_3_pred, c("0.025quant"="RW1_lower", "0.975quant"="RW1_upper","mean"="RW1_mean"))
+nb.ar1_3_pred <- reshape:::rename(nb.ar1_3_pred, c("0.025quant"="AR1_lower", "0.975quant"="AR1_upper","mean"="AR1_mean"))
+Day3_pred<-cbind(sa_cov_dat,nb.rw1_3_pred,nb.ar1_3_pred)
+
+nb.rw1_5_pred <- reshape:::rename(nb.rw1_5_pred, c("0.025quant"="RW1_lower", "0.975quant"="RW1_upper","mean"="RW1_mean"))
+nb.ar1_5_pred <- reshape:::rename(nb.ar1_5_pred, c("0.025quant"="AR1_lower", "0.975quant"="AR1_upper","mean"="AR1_mean"))
+Day5_pred<-cbind(sa_cov_dat,nb.rw1_5_pred,nb.ar1_5_pred)
+
+nb.rw1_7_pred <- reshape:::rename(nb.rw1_7_pred, c("0.025quant"="RW1_lower", "0.975quant"="RW1_upper","mean"="RW1_mean"))
+nb.ar1_7_pred <- reshape:::rename(nb.ar1_7_pred, c("0.025quant"="AR1_lower", "0.975quant"="AR1_upper","mean"="AR1_mean"))
+Day7_pred<-cbind(sa_cov_dat,nb.rw1_7_pred,nb.ar1_7_pred)
+
+#----Present the result as a table as well as plot 
+Day7_pred<-Day7_pred%>%
+  mutate(Cum_pred_ar1=cumsum(AR1_mean),Cum_pred_ar1_lower=cumsum(AR1_lower),Cum_pred_ar1_upper=cumsum(AR1_upper),
+         Cum_pred_rw1=cumsum(RW1_mean),Cum_pred_rw1_lower=cumsum(RW1_lower),Cum_pred_rw1_upper=cumsum(RW1_upper))
+Day5_pred<-Day5_pred%>%
+  mutate(Cum_pred_ar1=cumsum(AR1_mean),Cum_pred_ar1_lower=cumsum(AR1_lower),Cum_pred_ar1_upper=cumsum(AR1_upper),
+         Cum_pred_rw1=cumsum(RW1_mean),Cum_pred_rw1_lower=cumsum(RW1_lower),Cum_pred_rw1_upper=cumsum(RW1_upper))
+Day3_pred<-Day3_pred%>%
+  mutate(Cum_pred_ar1=cumsum(AR1_mean),Cum_pred_ar1_lower=cumsum(AR1_lower),Cum_pred_ar1_upper=cumsum(AR1_upper),
+         Cum_pred_rw1=cumsum(RW1_mean),Cum_pred_rw1_lower=cumsum(RW1_lower),Cum_pred_rw1_upper=cumsum(RW1_upper))
 
 
-# BRMS option
 
 
 
+forcast_cum<-Day7_pred%>%
+  ggplot(aes(y=total, x=date2)) +
+  geom_blank()+
+  geom_point(aes(y=total, x=date2)) +
+  geom_line(aes(y=Cum_pred_rw1, x=date2),linetype = "dashed", color="red") +
+  geom_line(aes(y=Cum_pred_ar1, x=date2),color="blue") +
+  xlab("Date")+
+  ylab("Cummulative observed and fitted cases")+
+  labs(title = "Negative-Binomial model - 7 days a head forcasting")+
+  theme_bw()
 
+forcast_cum<-forcast_cum+annotate("segment", x = as.Date("2020-10-06","%Y-%m-%d"), xend = as.Date("2021-02-06","%Y-%m-%d"), y = 1500000, yend = 1500000,
+         colour = "blue", size = 1, arrow = arrow())
+
+forcast_cum_zoom<-Day7_pred[Day7_pred$time>324,]%>%
+  ggplot(aes(y=total, x=date2)) +
+  geom_blank()+
+  geom_point(aes(y=total, x=date2)) +
+  geom_line(aes(y=Cum_pred_rw1, x=date2),linetype = "dashed", color="red") +
+  geom_line(aes(y=Cum_pred_ar1, x=date2),color="blue") +
+  ylab(" ")+
+  xlab("Date")+
+  theme_bw()
+
+forcast_cum_all<-forcast_cum + inset_element(forcast_cum_zoom, left =0.3, right =0.7, bottom = 0.6, top =0.99)
+
+# table 
+Frocasting7<-Day7_pred[Day7_pred$time>335,c("date2","total","Cum_pred_rw1", "Cum_pred_rw1_lower", "Cum_pred_rw1_upper"
+                                            ,"Cum_pred_ar1", "Cum_pred_ar1_lower", "Cum_pred_ar1_upper")]
+
+Frocasting7$Pred_Int_rw1<-paste0("(",round(Frocasting7$Cum_pred_rw1_lower,digits=2), "-", round(Frocasting7$Cum_pred_rw1_upper,digits=2),")")
+Frocasting7$Pred_Int_ar1<-paste0("(",round(Frocasting7$Cum_pred_ar1_lower,digits=2), "-", round(Frocasting7$Cum_pred_ar1_upper,digits=2),")")
+
+Frocasting7[,c("date2","total","Cum_pred_rw1","Pred_Int_rw1","Cum_pred_ar1","Pred_Int_ar1")]
 
